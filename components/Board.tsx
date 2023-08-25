89181db4616d9dc9371193cf8e5a5f409997dacc
Joshardals <joshuabamidele219@gmail.com>
2023-08-25 18:41:03 +0100

    initial commit

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,13 +1,22 @@
 "use client";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div></div>}
-      </Droppable>
-    </DragDropContext>
+    <h1>hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div></div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 };
 
